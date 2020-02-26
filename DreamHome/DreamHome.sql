ef676f3f9684ef9342b0277a89d1d76c8272dc71
Nathan Robinson <nrobinson2000@me.com>
2020-02-26 02:22:41 -0500

    everything at 02:22 of 2020-02-26

--- DreamHome/DreamHome.sql
+++ DreamHome/DreamHome.sql
@@ -100,7 +100,61 @@ CREATE TABLE REGISTRATION_t
     CONSTRAINT REGISTRATION_FK1 FOREIGN KEY (Client_Number) REFERENCES CLIENT_t(Client_Number),
     CONSTRAINT REGISTRATION_FK2 FOREIGN KEY (Branch_Number) REFERENCES BRANCH_t(Branch_Number),
     CONSTRAINT REGISTRATION_FK3 FOREIGN KEY (Staff_Number) REFERENCES STAFF_t(Staff_Number)
-
 );
 
 -- Do insertions
+
+INSERT INTO BRANCH_t
+VALUES
+    ("B005", "22 Deer Rd", "London", "SW1 4EH"),
+    ("B007", "16 Argyll", "Aberdeen", "AB2 3SU"),
+    ("B003", "163 Main St", "Glasgow", "G11 9QX"),
+    ("B004", "32 Manse Rd", "Bristol", "BS99 1NZ"),
+    ("B002", "56 Clover Drive", "London", "NW10 6EU");
+
+INSERT INTO STAFF_t
+VALUES
+    ("SL21", "John", "White", "Manager", "M", "1945-10-01", 30000, "B005"),
+    ("SG37", "Ann", "Beech", "Assistant", "F", "1960-11-10", 12000, "B003"),
+    ("SG14", "David", "Ford", "Supervisor", "M", "1958-03-24", 18000, "B003"),
+    ("SA9", "Mary", "Howe", "Assistant", "F", "1970-02-19", 9000, "B007"),
+    ("SG5", "Susan", "Brand", "Manager", "F", "1940-06-03", 24000, "B003"),
+    ("SL41", "Julie", "Lee", "Assistant", "F", "1965-06-13", 9000, "B005");
+
+INSERT INTO PRIVATE_OWNER_t
+VALUES
+    ("CO46", "Joe", "Keogh", "2 Fergus Dr, Aberdeen AB2 7SX", "01224-861212"),
+    ("CO87", "Carol", "Farrel", "6 Achray St, Glasgow", "0141-357-7419"),
+    ("CO40", "Tina", "Murphy", "63 Well St, Glasgow G42", "0141-943-1728"),
+    ("CO93", "Tony", "Shaw", "12 Park Pl, Glasgow G4 0QR", "0141-225-7025");
+
+INSERT INTO PROPERTY_t
+VALUES
+    ("PA14", "16 Holhead", "Aberdeen", "AB7 5SU", "House", 6, 650, "CO46", "SA9", "B007"),
+    ("PL94", "6 Argyll St", "London", "NW2", "Flat", 4, 400, "CO87", "SL41", "B005"),
+    ("PG4", "6 Lawrence St", "Glasgow", "G11 9QX", "Flat", 3, 350, "CO40", null, "B003"),
+    ("PG36", "2 Manor Road", "Glasgow", "G32 4QX", "Flat", 3, 375, "CO93", "SG37", "B003"),
+    ("PG21", "18 Dale Rd", "Glasgow", "G12", "House", 5, 600, "CO87", "SG37", "B003"),
+    ("PG16", "5 Novar Dr", "Glasgow", "G12 9AX", "Flat", 4, 450, "CO93", "SG14", "B003");
+
+INSERT INTO CLIENT_t
+VALUES
+    ("CR76", "John", "Kay", "0207-774-5632", "Flat", 425),
+    ("CR56", "Aline", "Stewart", "0141-848-1825", "Flat", 350),
+    ("CR74", "Mike", "Ritchie", "01475-392178", "House", 750),
+    ("CR62", "Mary", "Tregear", "01224-196750", "Flat", 600);
+
+INSERT INTO VIEWING_t
+VALUES
+    ("CR56", "PA14", "2004-05-24", "too small"),
+    ("CR76", "PG4", "2004-04-20", "too remote"),
+    ("CR56", "PG4", "2004-05-26", null),
+    ("CR62", "PA14", "2004-05-14", "no dining room"),
+    ("CR56", "PG36", "2004-04-28", null);
+
+INSERT INTO REGISTRATION_t
+VALUES
+    ("CR76", "B005", "SL41", "2004-01-02"),
+    ("CR56", "B003", "SG37", "2003-04-11"),
+    ("CR74", "B003", "SG37", "2003-11-16"),
+    ("CR62", "B007", "SA9", "2003-03-07");
