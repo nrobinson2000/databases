61536eb2c65140279b2119bc2080346ee4982746
Angel Carboni <48259942+carboniaatwit@users.noreply.github.com>
2020-03-17 15:11:35 -0400

    Update insertions.sql

--- project/insertions.sql
+++ project/insertions.sql
@@ -15,7 +15,7 @@ VALUES
     ("W00381023", "Billy McLellan", "Electromechanical Engineering", "2021"),
     ("W00123456", "Billy Bob", "Architecture", "2020"),
     ("W00987654", "Susie Lee", "Interior Design", "2023"),
-    ("W00", "Bryant Gill", "Major", "GradYear");
+    ("W00392828", "Bryant Gill", "Mechanical Engineering", "2021");
 
 INSERT INTO Club
 VALUES
@@ -34,7 +34,7 @@ VALUES
     ("Costume Club", "W00398867"),
     ("Costume Club", "W00381023"),
     ("Costume Club", "W00387654"),
-    ("Costume Club", "W00"),
+    ("Costume Club", "W00392828"),
     ("Robotics Club", "W00389606"),
     ("Robotics Club", "W00379455"),
     ("Robotics Club", "W00381023"),
@@ -63,23 +63,23 @@ VALUES
 
 INSERT INTO Project_Assignment
 VALUES
-    ("A01", "Magic Mirror", "Tory Lam", "W00", "08-01-2020", "13-04-2020"),
-    ("A02", "Project: Take Over", "Tory Lam", "W00123456", "10-10-2018", "07-12-2025"),
-    ("A03", "Cellular Automation", "Micah Schuster", "W00387654", "08-01-2020", "13-04-2020"),
+    ("A01", "Magic Mirror", "Tory Lam", "W00392828", "2020-01-08", "2020-04-13"),
+    ("A02", "Project: Take Over", "Tory Lam", "W00123456", "2018-10-10", "2025-12-07"),
+    ("A03", "Cellular Automation", "Micah Schuster", "W00387654", "2020-01-08", "2020-04-13"),
 
 INSERT INTO Staff
 VALUES
     ("S01", "W00123456", "CO-OP", 40),
     ("S02", "W00395778", "Work Study", 15),
     ("S03", "W00987654", "Work Study", 10),
-    ("S04", "W00", "CO-OP", 40);
+    ("S04", "W00392828", "CO-OP", 40);
 
 INSERT INTO Events
 VALUES
-    ("E01", "ThinkTank: Future of Making", "19-10-2019", "10:00:00"),
-    ("E02", "Project Showcase", "12-09-2019", "12:00:00"),
-    ("E03", "ThinkTank: Future of Our Cities", "30-04-2018", "10:00:00"),
-    ("E04", "Pitch Night", "04-09-2019", "17:00:00");
+    ("E01", "ThinkTank: Future of Making", "2019-10-19", "10:00:00"),
+    ("E02", "Project Showcase", "2019-09-12", "12:00:00"),
+    ("E03", "ThinkTank: Future of Our Cities", "2018-04-30", "10:00:00"),
+    ("E04", "Pitch Night", "2019-09-04", "17:00:00");
 
 INSERT INTO Staff_List
 VALUES
@@ -102,37 +102,37 @@ VALUES
 
 INSERT INTO Checkout
 VALUES
-    ("C001", "LT001", "W00", "15-01-2020"),
-    ("C002", "LT003", "W00", "15-01-2020"),
-    ("C003", "LT001", "W00123456", "09-03-2020");
+    ("C001", "LT001", "W00389606", "2020-01-15"),
+    ("C002", "LT003", "W00400205", "2020-01-15"),
+    ("C003", "LT001", "W00123456", "2020-03-09");
 
 INSERT INTO Tool
 VALUES
-    ("T001", "TrainingRequired", "ToolDescription"),
-    ("T002", "TrainingRequired", "ToolDescription"),
-    ("T003", "TrainingRequired", "ToolDescription"),
-    ("T004", "TrainingRequired", "ToolDescription"),
-    ("T005", "TrainingRequired", "ToolDescription");
+    ("T001", FALSE, "Hammer"),
+    ("T002", FALSE, "Large paper cutter"),
+    ("T003", FALSE, "Hot glue gun"),
+    ("T004", TRUE, "laser Cutter"),
+    ("T005", TRUE, "Belt Sander");
 
 INSERT INTO Material
 VALUES
-    ("M00", "MaterialDescription"),
-    ("M00", "MaterialDescription"),
-    ("M00", "MaterialDescription"),
-    ("M00", "MaterialDescription"),
-    ("M00", "MaterialDescription");
+    ("M001", "Velcro"),
+    ("M002", "Polyfil"),
+    ("M003", "Safety pins"),
+    ("M004", "Bread boards"),
+    ("M005", "Hot glue gun refills");
 
 INSERT INTO Filament
 VALUES
-    ("F0", "FilamentBrand", "FilamentColor", "FilamentDescription"),
-    ("F0", "FilamentBrand", "FilamentColor", "FilamentDescription"),
-    ("F0", "FilamentBrand", "FilamentColor", "FilamentDescription"),
-    ("F0", "FilamentBrand", "FilamentColor", "FilamentDescription"),
-    ("F0", "FilamentBrand", "FilamentColor", "FilamentDescription");
+    ("F00", "MakerBot", "white", "white PLA filament for 3D printing"),
+    ("F01", "MakerBot", "grey", "grey PLA filament for 3D printing"),
+    ("F02", "MakerBot", "black", "black PLA filament for 3D printing"),
+    ("F03", "MakerBot", "blue", "blue PLA filament for 3D printing"),
+    ("F04", "MakerBot", "purple", "purple PLA filament for 3D printing");
 
 INSERT INTO 3DPrinter
 VALUES
-    ("P0", "F0", "PrinterBrand", "PrinterDescription"),
-    ("P0", "F0", "PrinterBrand", "PrinterDescription"),
-    ("P0", "F0", "PrinterBrand", "PrinterDescription"),
-    ("P0", "F0", "PrinterBrand", "PrinterDescription");
+    ("P01", "F01", "MakerBot", "3D Printer using filament"),
+    ("P02", "F04", "MakerBot", "3D Printer using filament"),
+    ("P03", "null", "Prusa", "3D Printer using resin"),
+    ("P04", "null", "Prusa", "3D Printer using resin");
