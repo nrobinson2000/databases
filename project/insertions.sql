978ad0a644178439e81b499c800c4ba377098987
Nathan Robinson <nrobinson2000@me.com>
2020-03-30 22:44:11 -0400

    composite foreign key PRINTER_FK2 at 22:44 of 2020-03-30

--- project/insertions.sql
+++ project/insertions.sql
@@ -298,6 +298,7 @@ VALUES
     "0017",
     "3",
     "0012",
+    "F",
     "MakerBot",
     "3D Printer using filament"
   ),
@@ -305,6 +306,7 @@ VALUES
     "0018",
     "3",
     "0013",
+    "F",
     "MakerBot",
     "3D Printer using filament"
   ),
@@ -312,6 +314,7 @@ VALUES
     "0019",
     "3",
     "0015",
+    "F",
     "Prusa",
     "3D Printer using resin"
   ),
@@ -319,6 +322,7 @@ VALUES
     "0020",
     "3",
     "0016",
+    "F",
     "Prusa",
     "3D Printer using resin"
   );
